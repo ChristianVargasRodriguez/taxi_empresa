4b546e282ab3f9219d42a9d02735987e89a37e6b
Christian Vargas <christianj.vrgs@gmail.com>
2023-03-27 17:54:18 -0300

    SQL modificado

---
+++ dumps/taxi_empresa.sql
@@ -0,0 +1,82 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema taxi_empresa
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema taxi_empresa
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `taxi_empresa` DEFAULT CHARACTER SET utf8 ;
+USE `taxi_empresa` ;
+
+-- -----------------------------------------------------
+-- Table `taxi_empresa`.`usuarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taxi_empresa`.`usuarios` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NULL,
+  `apellido` VARCHAR(255) NULL,
+  `empresa` VARCHAR(255) NULL,
+  `cargo` VARCHAR(255) NULL,
+  `telefono` TINYINT(11) NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(255) NULL,
+  `created_at` DATETIME NULL DEFAULT NOW(),
+  `updated_at` DATETIME NULL DEFAULT NOW(),
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `taxi_empresa`.`conductores`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taxi_empresa`.`conductores` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(255) NULL,
+  `apellido` VARCHAR(255) NULL,
+  `empresa` VARCHAR(255) NULL,
+  `es_conductor` VARCHAR(255) NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(255) NULL,
+  `created_at` DATETIME NULL DEFAULT NOW(),
+  `updated_at` DATETIME NULL DEFAULT NOW(),
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `taxi_empresa`.`viajes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taxi_empresa`.`viajes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `direccion_inicio` VARCHAR(255) NULL,
+  `direccion_destino` VARCHAR(255) NULL,
+  `detalles` TEXT NULL,
+  `usuario_id` INT NOT NULL,
+  `conductor_id` INT NULL,
+  `created_at` DATETIME NULL DEFAULT NOW(),
+  `updated_at` DATETIME NULL DEFAULT NOW(),
+  PRIMARY KEY (`id`),
+  INDEX `fk_viajes_conductor_idx` (`conductor_id` ASC) VISIBLE,
+  INDEX `fk_viajes_usuarios1_idx` (`usuario_id` ASC) VISIBLE,
+  CONSTRAINT `fk_viajes_conductor`
+    FOREIGN KEY (`conductor_id`)
+    REFERENCES `taxi_empresa`.`conductores` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_viajes_usuarios1`
+    FOREIGN KEY (`usuario_id`)
+    REFERENCES `taxi_empresa`.`usuarios` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
